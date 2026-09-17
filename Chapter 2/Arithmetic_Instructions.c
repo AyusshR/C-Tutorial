7d11c5ddf3c2ac2361944f7473a8f59db4f1436c
AyusshR <ayu09102006@gmail.com>
2026-09-17 21:31:46 +0530

    Create Arithmetic_Instructions.c

---
+++ Chapter 2/Arithmetic_Instructions.c
@@ -0,0 +1,11 @@
+/* int op int = int
+float op int = float
+float op float = float         */
+#include <stdio.h>
+
+int main() {
+    int x = (int) 1.999999;
+    printf("%f\n", 5.00/5);
+    printf("%f", x);
+    return 0;
+}
